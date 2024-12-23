a5b9367cba1949049b89cb374db1df0bd44f225d
guspaz0 <gusti.paz@gmail.com>
2024-12-23 00:50:39 +0000

    feat bonus 5 fees implemented

--- test/TokenFarm.test.ts
+++ test/TokenFarm.test.ts
@@ -1,13 +1,14 @@
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 import { expect } from 'chai';
 import { web3, network } from 'hardhat';
-import TokenFarmArtifact from '../artifacts/contracts/TokenFarm.sol/TokenFarm.json';
+import TokenFarmArtifact from '../artifacts/contracts/TokenFarmV2.sol/TokenFarmV2.json';
 import deployLpTokenFixture from './LpToken.test';
 import deployDappTokenFixture from './DappToken.test';
 
-describe('TokenFarm', function () {
+describe('TokenFarmV2', function () {
 
-	let REWARD_PER_BLOCK = {10: 1e9, 100: 1e12, 1000: 1e18}
+	let RewardPerBlock = {10: 1e9, 100: 1e12, 1000: 1e18}
+	const fee = 3;
 
 	async function deployTokenFarmFixture() {
 		const { DappToken } = await loadFixture(deployDappTokenFixture)
@@ -21,9 +22,10 @@ describe('TokenFarm', function () {
 			arguments: [
 				DappToken.options.address, 
 				LpToken.options.address,
-				REWARD_PER_BLOCK[10], //reward per block in range 10
-				REWARD_PER_BLOCK[100], //reward per block in range 100
-				REWARD_PER_BLOCK[1000]  //reward per block in range 1000
+				fee, //fee
+				RewardPerBlock[10], //reward per block in range 10
+				RewardPerBlock[100], //reward per block in range 100
+				RewardPerBlock[1000]  //reward per block in range 1000
 			],
 		});
 
@@ -123,13 +125,13 @@ describe('TokenFarm', function () {
 				let rewardPerBlock;
 				const blocksPassed = Number(blockNumber) - Number(block);
 				if (blocksPassed <= 10) {
-					rewardPerBlock = REWARD_PER_BLOCK[10]
+					rewardPerBlock = RewardPerBlock[10]
 				} else if (blocksPassed <= 100) {
-					rewardPerBlock = REWARD_PER_BLOCK[100]
+					rewardPerBlock = RewardPerBlock[100]
 				} else if (blocksPassed <= 1000) {
-					rewardPerBlock = REWARD_PER_BLOCK[1000]
+					rewardPerBlock = RewardPerBlock[1000]
 				}
-				//const REWARD_PER_BLOCK = await tokenFarm.methods.REWARD_PER_BLOCK().call({from: deployer});
+
 				const participation = Number(amount) / Number(totalStakingBalance);
 				const userReward = participation * Number(rewardPerBlock) * blocksPassed;
 				estimatedTotalRewards += userReward;
@@ -163,7 +165,7 @@ describe('TokenFarm', function () {
 			const userinfo = await tokenFarm.methods.users(user.address).call({from: user.address});
 			expect(Number(userinfo.stackingBalance)).to.be.equal(0)
 		})
-		it("after user had been called withdraw TokenFarm method, the raise in balance of LpToken must be the same amount",async function(){
+		it("after user had been called withdraw TokenFarm method, the raise in balance of LpToken must be the amount",async function(){
 			const user = userTransactions[1]
 			const lpBalance = await lpToken.methods.balanceOf(user.address).call({from: user.address})
 			expect(Number(user.amount)).to.be.equal(Number(lpBalance))
@@ -184,5 +186,20 @@ describe('TokenFarm', function () {
 			expect(Number(updatedReward.events.RangeRewardUpdated.returnValues._reward)).to.be.equal(1e15);
 		}) 
 	})
-
+	
+	describe("El Owner de TokenFarm puede retirar los fee obtenidos por las recompensas de los usuarios", function() {
+		it("the fee tax must be a number greater than 0 and less than 100",async function(){
+			const [owner] = await web3.eth.getAccounts();
+			const fee = await tokenFarm.methods.fee().call({from: owner});
+			expect(fee).to.be.gt(0).and.lt(100)
+		})
+		it("after the owner call withdrawFee, it should receive the amount in DappTokens", async function() {
+			const owner = await tokenFarm.methods.owner().call();
+			const balanceBefore = await dappToken.methods.balanceOf(owner).call({from: owner});
+			const withdrawFee = await tokenFarm.methods.withdrawFee().send({from: owner});
+			const balanceAfter = await dappToken.methods.balanceOf(owner).call({from: owner});
+			console.log('fee: '+ Number(withdrawFee.events.FeeClaimed.returnValues._amount))
+			expect(Number(withdrawFee.events.FeeClaimed.returnValues._amount)).to.be.equal(Number(balanceAfter)-Number(balanceBefore))
+		})
+	}) 
 });
